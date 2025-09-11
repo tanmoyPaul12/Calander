4b5e72b7eed737af790fae669f7dea05dc3b97ff
tanmoyPaul12 <tanmoy01paul@gmail.com>
2025-09-12 00:31:36 +0530

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+const monthEle = document.getElementById("month-name");
+const dayEle = document.getElementById("day-name");
+const dateEle = document.getElementById("date");
+const yearEle = document.getElementById("yr");
+
+
+const date = new Date();
+// console.log(date);
+const month = date.getMonth();
+
+monthEle.innerText = date.toLocaleString("en",{month:"long"})
+dayEle.innerText = date.toLocaleString("en",{weekday:"long"})
+dateEle.innerText = date.getDate();
+yearEle.innerText = date.getFullYear();
